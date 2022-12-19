a3b9db231ec33de2892e6ca51dfda567f386ff1d
Tyler Towery <tylertowery21@gmail.com>
2022-12-19 11:27:53 -0800

    complete all ES6 classes for different bee types

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,17 @@
-class HoneyMakerBee {
-  // TODO..
-};
+class HoneyMakerBee extends Bee {
+  constructor(age, color, food, job) {
+    super(color, food);
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
+
+  }
+
+  makeHoney() {
+    this.honeyPot++;
+  }
+
+  giveHoney() {
+    this.honeyPot--;
+  }
+}
--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,18 @@
-class RetiredForagerBee {
-  // TODO..
-};
+class RetiredForagerBee extends ForagerBee {
+  constructor(age, food, color, job, canFly, treasureChest) {
+    super(food, treasureChest);
+    this.age = 40;
+    this.color = 'grey';
+    this.job = 'gamble';
+    this.canFly = false;
+
+  }
+
+  forage() {
+    return 'I am too old, let me play cards instead';
+  }
+
+  gamble(treasure) {
+    this.treasureChest.push(treasure);
+  }
+}
